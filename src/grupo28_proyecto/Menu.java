dfd55d8ba7e7259d77cd56c78f25fd8919329248
AndrésBaltodano <AndrsBaltodano@DESKTOP-645ESKH>
2026-02-15 12:33:30 -0600

    commit inicial

---
+++ src/grupo28_proyecto/Menu.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo28_proyecto;
+
+/**
+ *
+ * @author AndrésBaltodano
+ */
+public class Menu {
+    public static void desplegarMenu(){
+        // desplegar el menú de opciones
+        /* MENU PRINCIPAL
+        1. Agregar animal
+        2. Eliminar animal
+        3. Asignar animal
+        4. Mover animal
+        5. Buscar animal
+        6. Crear reporte
+        0. Salir
+        */
+    }
+    
+    public static void opcionAgregarAnimal() {
+        
+    }
+}
